18610c28ab73d6555e36ef6384dbca80328ef10b
Aditi Bhosale <aditibhosale470@gmail.com>
2026-08-29 22:00:55 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+char div;
+int rollno,age;
+cout<<"Enter your age: ";
+cin>>age;
+cout<<"\nEnter your roll no.: ";
+cin>>rollno;
+cout<<"\nEnter your division: ";
+cin>>div;
+cout<<"\nYour Age: "<<age<<endl;
+cout<<"\nYour Roll no.: "<<rollno<<endl;
+cout<<"\nYour Division: "<<div<<endl;
+return 0;
+}
+
+
