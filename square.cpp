e160db59481e7f7174a9c9ec58320d52722485f9
Aditi Bhosale <aditibhosale470@gmail.com>
2026-09-01 19:56:41 +0530

    Add files via upload

---
+++ square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,s;
+    cout<<"\nEnter number: ";
+    cin>>a;
+    s=a*a;
+    cout<<"\n Square of the number is = "<<s;
+    return 0;
+}
