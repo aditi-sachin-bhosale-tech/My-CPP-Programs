0d4f1c5cfc9ae98034ea801dbf4eedc16e9bab82
Aditi Bhosale <aditibhosale470@gmail.com>
2026-09-01 19:55:39 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"\n Enter Two Numbers: ";
+    cin>>a>>b;
+    cout<<"\nAddition= "<<a+b;
+    cout<<"\nSubtraction= "<<a-b;
+    cout<<"\nMultiplication= "<<a*b;
+    cout<<"\nDivision= "<<a/b;
+    return 0;
+    
+}
